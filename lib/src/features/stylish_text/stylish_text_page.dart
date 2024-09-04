fc49b7f92dcdd78dd680e0bc7a81d4fb7e86f1c4
ruhulmath08 <amin.brainstation23@bkash.com>
2024-09-05 00:22:43 +0600

    Add flutter stylish_text

--- lib/src/features/stylish_text/stylish_text_page.dart
+++ lib/src/features/stylish_text/stylish_text_page.dart
@@ -14,8 +14,35 @@ class StylishTextPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text('Flutter Stylish Text'),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: ShaderMask(
+            shaderCallback: (bound) => const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment(0.8, 1),
+              colors: <Color>[
+                Color(0xff1f005c),
+                Color(0xff5b0060),
+                Color(0xff87016e),
+                Color(0xffac255e),
+                Color(0xffcad85c),
+                Color(0xffe16b5c),
+                Color(0xfff39060),
+                Color(0xffffb56b),
+              ],
+            ).createShader(bound),
+            child: const Text(
+              'Flutter Stylish Text',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 96,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
       ),
     );
   }
