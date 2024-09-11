0b088bcd1059f25162268cc15564d8ea37242f0a
ruhulmath08 <amin.brainstation23@bkash.com>
2024-09-11 18:16:22 +0600

    Add String constants

--- lib/src/core/constants/string_constants.dart
+++ lib/src/core/constants/string_constants.dart
@@ -1,4 +1,18 @@
 class StringConstants {
+  ///Login
+  static const String login = "Login";
+  static const String username = "Username";
+  static const String password = "Password";
+  static const String forgotPassword = "Forgot Password?";
+  static const String doNotHaveAnAccount = "Forgot Password";
+  static const String usernameIsRequired = "Username is required";
+  static const String passwordIsRequired = "Password is required";
+
+  ///SignUp
+  static const String signUp = "Sign Up";
+
+  ///Home
+  static const String home = "Home";
   static const String stylishText = "Stylish Text";
   static const String badgeWithIconButton = "Badge With Icon Button";
   static const String multiTapDetector = "Multi Tap Detector";
